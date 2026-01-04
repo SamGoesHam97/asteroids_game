9b8d8997f48660fad0d4bd7efed5aed219ad5791
samgoesham <samhinton97@gmail.com>
2026-01-04 13:06:00 -0600

    Added "clock" and "dt" variables. Called .tick() on clock with arguement of 60 to limit frames.

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)
+        dt = dt / 1000
+        
         print("Starting Asteroids with pygame version: 2.6.1")
         print("Screen width: 1280")
         print("Screen height: 720")
