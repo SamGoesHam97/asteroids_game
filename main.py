7d5fb8491da373b4b9ad9ca5fa07309c63b9c002
samgoesham <samhinton97@gmail.com>
2026-01-04 17:44:45 -0600

    Changed player.update(dt) to updatable.update(dt) and changed player.draw(screen) to "for sprite in drawable: sprite.draw(screen)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -14,13 +19,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            
+        updatable.update(dt)
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)
         dt = dt / 1000
-        
+
         print("Starting Asteroids with pygame version: 2.6.1")
         print("Screen width: 1280")
         print("Screen height: 720")
